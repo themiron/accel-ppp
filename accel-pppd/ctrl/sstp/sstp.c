themiron/accel-ppp

commit 635ab1b77b06a8891479a46a0e1ba88315ff3958

1. Fixes the issue #124 "HTTP replay for non SSTP query" 2. Fixes log_debug2 in Triton lib

accel-pppd/ctrl/sstp/sstp.c
@@ -1925,6 +1925,44 @@ static int sstp_handler(struct sstp_conn_t *conn, struct buffer_t *buf)
 	return 0;
 }
 
+static int sstp_write(struct triton_md_handler_t *h)
+{
+	struct sstp_conn_t *conn = container_of(h, typeof(*conn), hnd);
+	struct buffer_t *buf;
+	int n;
+
+	while (!list_empty(&conn->out_queue)) {
+		buf = list_first_entry(&conn->out_queue, typeof(*buf), entry);
+		while (buf->len) {
+			n = conn->stream->write(conn->stream, buf->head, buf->len);
+			if (n < 0) {
+				if (errno == EINTR)
+					continue;
+				if (errno == EAGAIN)
+					goto defer;
+				if (conf_verbose && errno != EPIPE)
+					log_ppp_info2("sstp: write: %s\n", strerror(errno));
+				goto drop;
+			} else if (n == 0)
+				goto defer;
+			buf_pull(buf, n);
+		}
+		list_del(&buf->entry);
+		free_buf(buf);
+	}
+
+	triton_md_disable_handler(h, MD_MODE_WRITE);
+	return 0;
+
+defer:
+	triton_md_enable_handler(h, MD_MODE_WRITE);
+	return 0;
+
+drop:
+	triton_context_call(&conn->ctx, (triton_event_func)sstp_disconnect, conn);
+	return 1;
+}
+
 static int sstp_read(struct triton_md_handler_t *h)
 {
 	struct sstp_conn_t *conn = container_of(h, typeof(*conn), hnd);
@@ -1956,6 +1994,7 @@ static int sstp_read(struct triton_md_handler_t *h)
 	return 0;
 
 drop:
+	n = sstp_write(h);
 	sstp_disconnect(conn);
 	return 1;
 }
@@ -2046,44 +2085,6 @@ static int sstp_recv(struct triton_md_handler_t *h)
 
 drop:
 	sstp_disconnect(conn);
-	return 1;
-}
-
-static int sstp_write(struct triton_md_handler_t *h)
-{
-	struct sstp_conn_t *conn = container_of(h, typeof(*conn), hnd);
-	struct buffer_t *buf;
-	int n;
-
-	while (!list_empty(&conn->out_queue)) {
-		buf = list_first_entry(&conn->out_queue, typeof(*buf), entry);
-		while (buf->len) {
-			n = conn->stream->write(conn->stream, buf->head, buf->len);
-			if (n < 0) {
-				if (errno == EINTR)
-					continue;
-				if (errno == EAGAIN)
-					goto defer;
-				if (conf_verbose && errno != EPIPE)
-					log_ppp_info2("sstp: write: %s\n", strerror(errno));
-				goto drop;
-			} else if (n == 0)
-				goto defer;
-			buf_pull(buf, n);
-		}
-		list_del(&buf->entry);
-		free_buf(buf);
-	}
-
-	triton_md_disable_handler(h, MD_MODE_WRITE);
-	return 0;
-
-defer:
-	triton_md_enable_handler(h, MD_MODE_WRITE);
-	return 0;
-
-drop:
-	triton_context_call(&conn->ctx, (triton_event_func)sstp_disconnect, conn);
 	return 1;
 }
 

accel-pppd/triton/triton.c
@@ -55,7 +55,7 @@ static __thread struct triton_context_t *this_ctx;
 static __thread jmp_buf jmp_env;
 static __thread void *thread_frame;
 
-#define log_debug2(fmt, ...)
+//#define log_debug2(fmt, ...)
 
 void triton_thread_wakeup(struct _triton_thread_t *thread)
 {

commit be4aa4f1d186a69a63112a82352ff61a40a2090b

sstp: flush queued output on disconnect
Drain out_queue to the stream in sstp_disconnect before closing, so a
queued response is sent before the connection is torn down. Best-effort,
non-blocking, via a sstp_flush() helper that mirrors sstp_write.

Fixes: 635ab1b7

accel-pppd/ctrl/sstp/sstp.c
@@ -2124,6 +2124,30 @@ static int sstp_send(struct sstp_conn_t *conn, struct buffer_t *buf)
 	return 0;
 }
 
+static void sstp_flush(struct sstp_conn_t *conn)
+{
+	struct buffer_t *buf;
+	int n;
+
+	while (!list_empty(&conn->out_queue)) {
+		buf = list_first_entry(&conn->out_queue, typeof(*buf), entry);
+		while (buf->len) {
+			n = conn->stream->write(conn->stream, buf->head, buf->len);
+			if (n < 0) {
+				if (errno == EINTR)
+					continue;
+				if (conf_verbose && errno != EPIPE)
+					log_ppp_info2("sstp: flush: %s\n", strerror(errno));
+				break;
+			} else if (n == 0)
+				break;
+			buf_pull(buf, n);
+		}
+		list_del(&buf->entry);
+		free_buf(buf);
+	}
+}
+
 static void sstp_msg_echo(struct triton_timer_t *t)
 {
 	struct sstp_conn_t *conn = container_of(t, typeof(*conn), hello_timer);
@@ -2225,6 +2249,7 @@ static void sstp_disconnect(struct sstp_conn_t *conn)
 		triton_timer_del(&conn->hello_timer);
 
 	if (conn->hnd.tpd) {
+		sstp_flush(conn);
 		triton_md_unregister_handler(&conn->hnd, 0);
 		conn->stream->close(conn->stream);
 	}